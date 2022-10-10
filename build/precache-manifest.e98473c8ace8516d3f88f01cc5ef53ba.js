d73f77dc977ce3bea3b6439364872d30c0d2e977
bocchuoi <bocchuoi@gmail.com>
2022-10-09 22:00:55 -0400

    adding all the changes all at once which includes package.json work and app scr

---
+++ build/precache-manifest.e98473c8ace8516d3f88f01cc5ef53ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0431e8933b136550c625",
+    "url": "/bocchuoi/COMP3104/static/css/main.e0b845d3.chunk.css"
+  },
+  {
+    "revision": "0431e8933b136550c625",
+    "url": "/bocchuoi/COMP3104/static/js/main.0431e893.chunk.js"
+  },
+  {
+    "revision": "51fb781f12af9afd622a",
+    "url": "/bocchuoi/COMP3104/static/js/runtime~main.51fb781f.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/bocchuoi/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "62a81ede01ca6f6e3abf1855381a555c",
+    "url": "/bocchuoi/COMP3104/index.html"
+  }
+];
